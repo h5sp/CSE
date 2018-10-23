21898f540ec2f2f330ef0bc3fd1e6ca8685891c3
h5sp <42385415+h5sp@users.noreply.github.com>
2018-10-23 09:46:38 -0700

    Initial upload

--- notes/hello world.py
+++ notes/hello world.py
@@ -1,1 +1,28 @@
 print("Hello World!")
+
+# This is a comment. This has no effect on the code
+# but this does allow me to do things. I can:
+# 1. Make notes to myslef
+# 2. Comment pieces of the code that do not work
+# 3. Make my code easier to read
+
+print("Look what happens here. Is there any space?")
+print("")
+print("")
+print("There should be a couple blank lines here.")
+
+# Math
+print(3+5)
+print(5-2)
+print(3*4)
+print(6/2)
+
+print("Figure this out...")
+print(6 // 2)
+print(5 // 2)
+print(9 // 4)
+
+print("Here is another one... ")
+print(6 % 2)
+print(5 % 2)
+print(11 % 4)   # Module (Remainder)
