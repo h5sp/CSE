2caf40dc3f018d3bbfe94f4c750e252b60b3f2fa
h5sp <42385415+h5sp@users.noreply.github.com>
2018-11-05 09:46:03 -0800

    intial upload

--- Marbeisi-Mad Lib.py
+++ Marbeisi-Mad Lib.py
@@ -14,7 +14,25 @@ verb = input("choose a verb.")
 print("so I opened the basement door and %s down." % verb)
 
 print("a bit.")
+print("I didn’t see a puppy, and then Mommy yanked me out of the basement and yelled at me. ")
 
+emotion = input("choose an emotion.")
+print("Mommy had never yelled at me before, and it made me %s." % emotion)
 
+verb = input("choose a verb.")
+print("and I %s." % verb)
 
+food = input("Choose a type a food.")
+print("Then Mommy told me never to go into the basement again, and she gave me a %s." % food)
 
+emotion = input("Choose an emotion.")
+print("That made me feel %s." % emotion)
+
+person = input("choose a person.")
+print(", so I didn’t ask her why the %s in the basement was making noises like a puppy" % person)
+
+part of a body = input("choose a body part.")
+print(", or why he had no %s" % body part)
+
+part of a body = input("part of a body.")
+print("or %s." % body part)
