31f740e0d23474d5a9a2fef3f0e0edc6c06423d1
h5sp <42385415+h5sp@users.noreply.github.com>
2018-11-07 09:44:33 -0800

    inital upload

--- notes/Marbeisi -Guess game.py
+++ notes/Marbeisi -Guess game.py
@@ -1,1 +1,10 @@
 import random
+print(random.randint(0, 10))
+
+print("Guess what number I'm thinking of!")
+print("It's a number between 1 and 10")
+print("Good Luck!")
+
+number = input("Pick a number.")
+print("Ha! I don't think so %s isn't the correct answer."% number)
+
