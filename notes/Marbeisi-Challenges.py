b9289c15a5aaf042f115ecb6dba42692437bfa33
h5sp <42385415+h5sp@users.noreply.github.com>
2018-12-07 10:39:00 -0800

    Initial upload

--- notes/Marbeisi-Challenges.py
+++ notes/Marbeisi-Challenges.py
@@ -13,12 +13,46 @@ if (num % 2) == 0:
 else:
    print("{0} is Odd".format(num))
 
+
 # challenge3
 
+b = int(input("Input the base :"))
+h = int(input("Input the height :"))
 
 # challenge4
-
-
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Zero")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number.")
 # challenge5
+r = 3
+area = 3.14 * r ** 2
+print(area)
+
+# challenge 6
+r = 5
+volume = 1.33 * 3.44 * r ** 3
+print(volume)
+
+# challenge 7
+n = 3
+value = n + (n * n) + (n * n * n)
+
+# challenge 8
 
 
+# challenge 9
+
+
+def is_vowel(char):
+    all_vowels = 'aeiou'
+    return char in all_vowels
+
+
+print(is_vowel('c'))
+print(is_vowel('e'))
+print(is_vowel('c'))
+print(is_vowel('c'))
