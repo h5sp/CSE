6af2b57157d3347510efeaba0c7eabad652b9ea7
h5sp <42385415+h5sp@users.noreply.github.com>
2018-12-10 09:43:14 -0800

    Initial upload

--- notes/Marbeisi-Challenges.py
+++ notes/Marbeisi-Challenges.py
@@ -7,7 +7,15 @@ name1 = challenge1("Marbeisi", "Morales")
 print(name1)
 
 # challenge2
-num = int(input("Enter a number: "))
+
+
+def challenge2(number):
+    return number
+
+
+num = challenge2("Pick a number:")
+print(num)
+
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
@@ -32,27 +40,3 @@ r = 3
 area = 3.14 * r ** 2
 print(area)
 
-# challenge 6
-r = 5
-volume = 1.33 * 3.44 * r ** 3
-print(volume)
-
-# challenge 7
-n = 3
-value = n + (n * n) + (n * n * n)
-
-# challenge 8
-
-
-# challenge 9
-
-
-def is_vowel(char):
-    all_vowels = 'aeiou'
-    return char in all_vowels
-
-
-print(is_vowel('c'))
-print(is_vowel('e'))
-print(is_vowel('c'))
-print(is_vowel('c'))
