a750dfd8cd537c1c8c6620fb232393336818af59
h5sp <42385415+h5sp@users.noreply.github.com>
2019-01-08 09:44:43 -0800

    Initial upload

---
+++ Semester 2 notes.py
@@ -0,0 +1,22 @@
+print("HELLO WORLD!")
+
+# I LOVE FOOD
+
+cars = 5
+driving = True
+
+print(" I have %d cars!" % cars)
+print(" I have " + str(cars) + " cars")
+
+age = input("How old are you?")
+print("Damm you're %s you're super old!" % age)
+
+colors = ["Red", "Blue", "Orange", "Purple", "Black"]
+colors_append = ["Brown"]
+
+print(colors)
+colors.pop(0)
+print(colors)
+print(colors[3])
+
+print(len(colors))
