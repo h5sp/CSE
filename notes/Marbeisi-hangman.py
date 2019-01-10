20980e0f9fe235f75579533a9a77de52ac1d0f9d
h5sp <42385415+h5sp@users.noreply.github.com>
2019-01-10 09:43:44 -0800

    initial upload

--- notes/Marbeisi-hangman.py
+++ notes/Marbeisi-hangman.py
@@ -1,11 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
+print("Let's play hangman!")
+# Every single letter is in the list
+# If corect letter countdown stops nut if incorrect then goes down
+# 8 guess wrong
---
+++ notes/hangman test.py
@@ -0,0 +1,7 @@
+word = 'Look its a wildbeest'
+guesses = 8
+win = False
+string1 = 'Look its a wildebeest'
+list1 = list(string1)
+
+while guesses > 0:
