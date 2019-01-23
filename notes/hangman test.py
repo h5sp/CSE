38552d4ec64bedd23f407c7c1ab4eefa15201a05
h5sp <42385415+h5sp@users.noreply.github.com>
2019-01-23 09:43:38 -0800

    initial upload

--- notes/hangman test.py
+++ notes/hangman test.py
@@ -1,12 +1,13 @@
 import random
 import string
+
 words = ["Crypt", "Wildebeest", "Cupcake", "Documents", "Awkward", "Croquet", "Fervid",
          " Bungler", "Rhythmic", "Zombie"]
-print(random.choices(words))
 print(list(string.ascii_letters))
-
+print(random.choice(words))
 win = False
 guesses = 8
 secretWord = random.choice(words)
 guess_word = []
 length_word = len(secretWord)
+letter_storage = []
--- notes/test2.py
+++ notes/test2.py
@@ -1,36 +1,37 @@
 import random
 import sys
-
-
-# lets set some variables
-wordList = ["crypt", "wildebeest", "cupcake", "documents", "awkward", "croquet", "fervid",
-         "bungler", "rhythmic", "zombie"]
+wordList = ["crypt", "wildebeest", "cupcake", "documents", "awkward", "croquet", "fervid", "bungler", "rhythmic",
+            "zombie"]
 
 guess_word = []
-secretWord = random.choice(wordList) # lets randomize single word from the list
+secretWord = random.choice(wordList)
 length_word = len(secretWord)
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 letter_storage = []
 
-def newFunc():
+
+def newfunc():
     print("Well, that's perfect moment to play some Hangman!\n")
 
     while True:
-        gameChoice = input("Would You?\n").upper()
+        gamechoice = input("Would You?\n").upper()
 
-        if gameChoice == "YES" or gameChoice == "Y":
+        if gamechoice == "YES" or gamechoice == "Y":
             break
-        elif gameChoice == "NO" or gameChoice == "N":
+        elif gamechoice == "NO" or gamechoice == "N":
             sys.exit("That's a shame! Have a nice day")
         else:
             print("Please Answer only Yes or No")
             continue
-newFunc()
+
+
+newfunc()
+
 
 def change():
 
     for character in secretWord:
-        guess_word.append("*")
+        guess_word.append("-")
 
     print("Ok, so the word You need to guess has", length_word, "characters")
 
@@ -39,14 +40,10 @@ def change():
     print(guess_word)
 
 
-
-
 def guessing():
     guess_taken = 1
 
     while guess_taken < 8:
-
-
         guess = input("Pick a letter\n").lower()
 
         if not guess in alphabet:
@@ -63,7 +60,7 @@ def guessing():
                         guess_word[x] = guess
                         print(guess_word)
 
-                if not '*' in guess_word:
+                if 'not in' '*' in guess_word:
                     print("You won!")
                     break
             else:
@@ -71,7 +68,6 @@ def guessing():
                 guess_taken += 1
                 if guess_taken == 10:
                     print(" Sorry Mate, You lost :<! The secret word was",         secretWord)
-
 
 
 change()
