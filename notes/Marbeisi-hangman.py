4d53c201c314fd58631f16b25010b2ead90ea8ad
h5sp <42385415+h5sp@users.noreply.github.com>
2019-01-24 09:44:34 -0800

    initial upload

--- notes/Marbeisi-hangman.py
+++
@@ -1,6 +0,0 @@
-mainStr = "Hello peeps!"
-'''
-Replace all occurrences of given character or string in main string
-'''
-otherStr = mainStr.replace('o', 'p')
-print(otherStr)
--- notes/hangman test.py
+++ notes/hangman test.py
@@ -3,11 +3,31 @@ import string
 
 words = ["Crypt", "Wildebeest", "Cupcake", "Documents", "Awkward", "Croquet", "Fervid",
          " Bungler", "Rhythmic", "Zombie"]
-print(list(string.ascii_letters))
-print(random.choice(words))
+(list(string.ascii_letters))
+(random.choice(words))
 win = False
 guesses = 8
 secretWord = random.choice(words)
 guess_word = []
 length_word = len(secretWord)
 letter_storage = []
+guesses_left = 8
+letter_in_word = []
+list_of_guesses = []
+
+while guesses_left > 0:
+    guess = input("Enter a letter: ")
+
+    if guess in letter_in_word:
+        print("%s is in the word" % guess)
+    else:
+        print("%s is not in the word" % guess)
+
+    guesses_left -= 1
+    print("You have: ", guesses_left, "guesses so far")
+
+    list_of_guesses.append(guess)
+    print("Guesses made so far:", list_of_guesses)
+
+    if guesses_left == 0:
+        print("You ran out of guesses! :0!!")
