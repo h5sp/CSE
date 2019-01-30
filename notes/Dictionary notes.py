e7cfa32ddc360b4470f07ce176fe1dec6043ee74
h5sp <42385415+h5sp@users.noreply.github.com>
2019-01-30 09:44:26 -0800

    initial upload

--- notes/Dictionary notes.py
+++ notes/Dictionary notes.py
@@ -74,3 +74,25 @@ complex_dictionary = {
 }
 
 print(complex_dictionary["AK"]["CITIES"][0])
+
+print(complex_dictionary["FL"]["NAME"])
+print(complex_dictionary["GA"]["CITIES"][0])
+
+print(complex_dictionary.keys())
+print(complex_dictionary.items())
+print(nested_dictionary.items())
+
+for key, value in complex_dictionary.items():
+    print(key)
+    print(value)
+    print("-" * 20)
+
+
+# This is what makes it look pretty
+print()
+for state, info in complex_dictionary.items():
+    for label, stats in info.items():
+        print(label)
+        print(stats)
+        print("-" * 20)
+    print("-" * 20)
