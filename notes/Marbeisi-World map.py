487ef19f28ae2ca24d3b8fe07112b7c7df614f26
h5sp <42385415+h5sp@users.noreply.github.com>
2019-02-06 09:43:51 -0800

    initial upload

---
+++ notes/Marbeisi-World map.py
@@ -0,0 +1,17 @@
+world_map = {
+    'OFFICE': {
+        'NAME': "The office",
+        'DESCRIPTION': " This is the office there is a answering machine in front of you ",
+        'PATHS': {
+            'NORTH': "Pasillo Central"
+        }
+    },
+    'Pasillo Central': {
+        'NAME': " The Pasillo Central",
+        'DESCRIPTION': "It is very dark in here! ",
+        'PATHS': {
+            'WEST': ""
+            'EAST': ""
+        }
+    }
+}
