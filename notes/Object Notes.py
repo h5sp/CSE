2ce9dc1e5b058fe3ca693b4639c9b2d8aecf2d22
h5sp <42385415+h5sp@users.noreply.github.com>
2019-02-13 09:45:14 -0800

    initial upload

--- notes/Object Notes.py
+++ notes/Object Notes.py
@@ -1,35 +1,54 @@
+import Special_Random
+
+
 class Phone(object):
-     def __init__(self, carrier, charge_left):
-                # These are attributes that a phone has.
-                # These should all be relevant to our program
+    def __init__(self, carrier, charge_left=50):
+        # These are attributes that a phone has.
+        # These should all be relevant to our program
         self.screen = True
         self.camera = 2
         self.microphone = True
         self.carrier = carrier
         self.battery_left = charge_left
 
-     def charge(self, time):
-         self.battery_left += time
-         if self.battery_left > 100:
-             self.battery_left = 100
+    def charge(self, time):
+        self.battery_left += time
+        if self.battery_left > 100:
+            self.battery_left = 100
 
-     def make_call(self, duration):
-         if not self.screen:
+    def make_call(self, duration):
+        if not self.screen:
              print("You can't make a phone call.")
              print("Your screen is broken")
              return
-         battery_loss_per_minute = 5
-         if self.battery_left <= 0:
+        battery_loss_per_minute = 5
+        if self.battery_left <= 0:
              print("You can't. The phone is dead.")
          self.battery_left -= duration * battery_loss_per_minute
-         if self.battery_left < 0:
+        if self.battery_left < 0:
              self.battery_left = 0
              print("Your phone dies during the conversation")
          elif self.battery_left == 0:
              print(" Your phone dies at the end of the conversation.")
          else:
-             print(""
-    )
+             print("You successfully make the phone call.")
+             print("Your phone is now at %s" % self.battery_left)
 
+    def smash_phone(self):
+        print("SMASH!!!!!!!")
+        print("It broke!")
+        self.screen = False
 
+# Initialize Objects
+my_phone = Phone("ATT", 100)
+your_phone = Phone("Bell", 0)
+default_phone = Phone("Verizon")
 
+my_phone.make_call(60)
+my_phone.make_call(10)
+my_phone.charge(100)
+my_phone.make_call(10)
+your_phone.smash_phone()
+your_phone.make_call(1)
+
+print(Special_Random.RandomWiebe.my_random())
---
+++ notes/Special_Random.py
@@ -0,0 +1,7 @@
+import random
+
+
+class RandomWiebe:
+    @staticmethod
+    def my_random():
+        return random.radint(1, 100)
