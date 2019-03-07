4f935ae40cc38782984a24e5bf3dcaec8ae28da9
h5sp <42385415+h5sp@users.noreply.github.com>
2019-03-07 09:44:34 -0800

    inital upload

--- notes/Marbeisi-Items.py
+++ notes/Marbeisi-Items.py
@@ -1,18 +1,8 @@
 class Items(objects):
-    def __init__(self):
-        self.flashlight = flashlight
-        self.bat = bat
-        self.key = key
-        self.health = health
-        self.matches = matches
-        self.clothes = cloths
-        self.pitchfork = pitchfork
-        self.health_kit = health_kit
-        self.watch = watch
-        self.tickets = tickets
-        self.chicken_nuggets = chicken_nuggets
-        self.microwave = microwave
-        self.ketchup = ketchup
-        self.nails = nails
-        self.hammer = hammer
+    def __init__(self, name):
+        self.name = name
 
+
+class Weapon(weapons):
+    def __init__(self):
+        
