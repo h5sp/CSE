8ac44cc7c09a8c39e8d8977fcc022adf41f1b588
h5sp <42385415+h5sp@users.noreply.github.com>
2019-04-26 09:48:01 -0700

    Initial upload

--- notes/Csv notess.py
+++ notes/Csv notess.py
@@ -1,5 +1,35 @@
 import csv
 
+
+def validate(num: str):
+    if not all_16_digits(num):
+        return False
+    if divisible_by_2(num) and divisible_by_3(num):
+        return True
+    return False
+
+
+def divisible_by_3(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def divisible_by_2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
+
+def all_16_digits(num: str):
+    if len(num) == 16:
+        return True
+    else:
+        print("Not every number has 16 digits!!")
+        return False
+
 # with open("Book1.csv") as old_csv:
 #   with open("MyNewFile.csv", 'w', newline='') as new_csv:
 #       print("Writing file.....   ")
@@ -14,16 +44,29 @@ import csv
 # print(old_number)
 # print("OK")
 
-with open("Book1.csv") as old_csv:
+# with open("Book1.csv", 'r') as old_csv:
+# with open("MyNewFile.csv", 'w', newline='') as new_csv:
+# reader = csv.reader(old_csv)
+# writer = csv.writer(new_csv)
+# #print("Processing....")
+# for row in reader:5
+#  first_num = int(old_number[0])  # This is the first number
+# if first_num % 2 == 0:
+#  writer.writerow(row)
+# print(int(old_number) + 1)
+# print(old_number)
+# print("Done")
+
+
+with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing file.....   ")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
+        print("Processing....")
+
         for row in reader:
-            old_number = row[0] # This is a string
-            first_num = int(old_number[0]) # This is the first number
-            if first_num % 2 == 0:
+            # old_number = int(row[0]) + 1
+            old_num = row[0]
+            if validate(old_num):
                 writer.writerow(row)
-            # print(int(old_number) + 1)
-            # print(old_number)
-print("OK")
+print("Done")
