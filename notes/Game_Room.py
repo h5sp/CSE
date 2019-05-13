77db4803cc868e5c68026927342d9191eeb2f4ef
h5sp <42385415+h5sp@users.noreply.github.com>
2019-05-13 09:46:34 -0700

    Initial upload

--- notes/Game_Room.py
+++ notes/Game_Room.py
@@ -1,4 +1,5 @@
-world_map =
+world_map = {}
+
 
 class Room(object):
     def __init__(self, name, description, north=None, south=None, east=None, west=None):
@@ -14,7 +15,7 @@ class Room(object):
 
 Office = Room("Office", " This is the office there is a answering machine in front of you")
 Pasillo_Central = ("Pasillo Central", "It is very dark in here! ")
-Party_Room1 = ("Party Room 1", " This is where the younger kids eat and party ...ITS a big MESS.")
+Party_Room_1 = ("Party Room 1", " This is where the younger kids eat and party ...ITS a big MESS.")
 Party_Room2 = Room("Party Room 2", "The door is locked!")
 Party_Room3 = Room("Party Room 3", "There are a couple of batteries in here.")
 Party_Room4 = Room("Party Room 4", "There is a couple of tables here.")
@@ -32,7 +33,7 @@ Exit = Room("Exit", "I'M SORRY THERE IS NO EXIT!!")
 
 
 Office.north = Pasillo_Central
-Pasillo_Central.west = Party_Room1
+Pasillo_Central.west = Party_Room_1
 Pasillo_Central.east = Party_Room2
 Pasillo_Central.north_west = Party_Room3
 Pasillo_Central.north_east = Party_Room4
@@ -49,7 +50,6 @@ Dining_Room.south_east = Prize_Corner
 Prize_Corner.south = Kids_Cove
 Kids_Cove.north = Prize_Corner
 Prize_Corner.north_east = Exit
-
 
 
 class Player(object):
@@ -72,7 +72,8 @@ class Player(object):
         name_of_room = getattr(self.current_location, direction)
         return globals()[name_of_room]
 
-ME = Player("me", )
+
+ME = Player("me", 100, Office)
 
 
 class Enemies(object):
@@ -84,182 +85,192 @@ class Enemies(object):
         self.inflict_damage = inflict_damage
 
     def attack(self):
-        print("health:", Player.health)
+        print("health:", ME.health)
         print("enemy slaps you.")
-        Player.health -= self.inflict_damage
+        ME.health -= self.inflict_damage
         print("mike takes 10 health points")
         print("your current health level was 100. now it's:")
-        print(Player.health)
+        print(ME.health)
 
 
-mike = Enemies("mike", 10)
+mike = Enemies("mike", 50, 10)
 mike.attack()
+
 
 class Items(object):
 
-    def __init__(self):
+    def __init__(self, name, health):
         self.name = name
-        self.health = 100
+        self.health = health
         self.armor = 100
         self.inventory = 10
         self.energy = 100
         self.health_kit_inventory = 50
 
-        class Weapons(Items):
-            def __init__(self, damage):
-                super(Weapons, self).__init__(name)
-                self.damage = damage
-                self.inventory = []
-                if health <= damage:
-                    print("There is damage done!")
+class Weapons(Items):
+    def __init__(self, name, health, damage):
+        super(Weapons, self).__init__(name, health)
+        self.damage = damage
+        self.inventory = []
+        if health <= damage:
+            print("There is damage done!")
+class Baseballbat(Weapons):
+    def __init__(self, damage, inventory):
+        super(Baseballbat, self).__init__(damage, inventory)
+        self.damage -= 15
+        self.inventory += 5
+        print("You have swinged the bat!")
+        answer = input("Do you want to pick this up?")
+        print("You have answer %s , you have picked this item up!" % answer)
+        if answer.lower() in ['no']:
+            print("item is not picked up!")
+class Pitchfork(Weapons):
+    def __init__(self, damage, inventory):
+        super(Pitchfork, self).__init__(damage, inventory)
+        self.damage -= 20
+        self.inventory += 10
+        print("you hav picked up the pitchfork!")
+        answer = input("Do you want to pick this up?")
+        print("You have answer %s , you have picked this item up!" % answer)
+        if answer.lower() in ['no']:
+            print("item is not picked up!")
+class Hammer(Weapons):
+    def __init__(self, damage, inventory):
+        super(Hammer, self).__init__(damage, inventory)
+        self.damage -= 10
+        self.inventory += 4
+        print("you have used the hammer!")
+        answer = input("Do you want to pick this up?")
+        print("You have answer %s , you have picked this item up!" % answer)
+        if answer.lower() in ['no']:
+            print("item is not picked up!")
+class Sword(Weapons):
+    def __init__(self, damage, inventory):
+        super(Sword, self).__init__(damage, inventory)
+        self.damage -= 20
+        self.inventory += 7
+        print("You have used your sword")
+        answer = input("Do you want to pick this up?")
+        print("You have answer %s , you have picked this item up!" % answer)
+        if answer.lower() in ['no']:
+            print("item is not picked up!")
 
-            def baseballbat(self):
-                self.damage -= 15
-                self.inventory += 5
-                print("You have swinged the bat!")
-                answer = input("Do you want to pick this up?")
-                print("You have answer %s , you have picked this item up!" % answer)
-                if answer.lower() in ['no']:
-                    print("item is not picked up!")
+class Axe(Weapons):
+    def __init__(self, damage):
+        super(Axe, self).__init__(damage)
+        self.damage -= 15
+        answer = input("Do you want to pick this up?")
+        print("You have answer %s , you have picked this item up!" % answer)
+        if answer.lower() in ['no']:
+            print("item is not picked up!")
 
-            def pitchfork(self):
-                self.damage -= 20
-                self.inventory += 10
-                print("you hav picked up the pitchfork!")
-                answer = input("Do you want to pick this up?")
-                print("You have answer %s , you have picked this item up!" % answer)
-                if answer.lower() in ['no']:
-                    print("item is not picked up!")
+class Tools(Items):
+    def __init__(self, inventory):
+        super(Tools, self).__init__(name, health)
+        self.inventory = inventory
 
-            def hammer(self):
-                self.damage -= 10
-                self.inventory += 4
-                print("you have used the hammer!")
-                answer = input("Do you want to pick this up?")
-                print("You have answer %s , you have picked this item up!" % answer)
-                if answer.lower() in ['no']:
-                    print("item is not picked up!")
+    def nails(self):
+        self.inventory += 1
+        print("This has been added to your inventory!")
 
-            def sword(self):
-                self.damage -= 20
-                self.inventory += 7
-                print("You have used your sword")
-                answer = input("Do you want to pick this up?")
-                print("You have answer %s , you have picked this item up!" % answer)
-                if answer.lower() in ['no']:
-                    print("item is not picked up!")
+    def matches(self):
+        self.inventory += 3
+        print("This has been added to your inventory!")
 
-            def axe(self):
-                self.damage -= 15
-            answer = input("Do you want to pick this up?")
-            print("You have answer %s , you have picked this item up!" % answer)
-            if answer.lower() in ['no']:
-                print("item is not picked up!")
+    def key(self):
+        self.inventory += 1
+        print("This has been added to your inventory!")
 
-        class Tools(Items):
-            def __init__(self, inventory):
-                super(Tools, self).__init__(name)
-                self.inventory = inventory
+class Energy(object):
+    def __init__(self, energy):
+        super(Energy, self).__init__(name)
+        self.energy = energy
 
-            def nails(self):
-                self.inventory += 1
-                print("This has been added to your inventory!")
+    def food(self, energy):
+        self.energy = energy
 
-            def matches(self):
-                self.inventory += 3
-                print("This has been added to your inventory!")
+    def chickennuggets(self):
+        self.energy += 5
+        print("Your energy has been boosted up!")
 
-            def key(self):
-                self.inventory += 1
-                print("This has been added to your inventory!")
+    def energydrinks(self):
+        self.energy += 10
+        print("Your energy has been boosted up!")
 
-        class Energy(energy):
-            def __init__(self, energy):
-                super(Energy, self).__init__(name)
-                self.energy = energy
+class HealthKit(self, health):
+    def __init__(self):
+        super(HealthKit, self).__init__(name, health)
+        self.health_Kit_Inventory = 50
 
-            def food(self):
-                self.energy = energy
+    def bandages(self):
+        self.health_Kit_Inventory += 2
+        print("Your health kit has more stuff now!")
+        print("You can use any of this anytime!")
 
-            def chickennuggets(self):
-                self.energy += 5
-                print("Your energy has been boosted up!")
+    def medicine(self):
+        self.health_Kit_Inventory += 5
+        print("Your health kit has more stuff now!")
+        print("You can use any of this anytime!")
 
-            def energydrinks(self):
-                self.energy += 10
-                print("Your energy has been boosted up!")
+    def anesthetic(self):
+        self.health_Kit_Inventory += 5
+        self.health += 5
+        print("Your health kit has more stuff now!")
+        print("You can use any of this anytime!")
 
-        class HealthKit(healthkit, health):
-            def __init__(self):
-                super(HealthKit, self).__init__(name)
-                self.health_Kit_Inventory = 50
+    def scissors(self):
+        self.health_Kit_Inventory += 2
+        print("Your health kit has more stuff now!")
+        print("You can use any of this anytime!")
 
-            def bandages(self):
-                self.health_Kit_Inventory += 2
-                print("Your health kit has more stuff now!")
-                print("You can use any of this anytime!")
+class Electronics(object):
+    def __init__(self, on):
+        self.on = on
 
-            def medicine(self):
-                self.health_Kit_Inventory += 5
-                print("Your health kit has more stuff now!")
-                print("You can use any of this anytime!")
+    def microwave(self):
+        print("The flashlight is on!")
+        self.on = True
+        answer = input("turn off?>_")
+        if answer.lower() in ['yes']:
+            print("The flashlight is off!")
+            self.on = False
 
-            def anesthetic(self):
-                self.health_Kit_Inventory += 5
-                self.health += 5
-                print("Your health kit has more stuff now!")
-                print("You can use any of this anytime!")
+    def flashlight(self):
+        print("The flashlight is on!")
+        self.on = True
+        answer = input("turn off?>_")
+        if answer.lower() in ['yes']:
+            print("The flashlight is off!")
+            self.on = False
 
-            def scissors(self):
-                self.health_Kit_Inventory += 2
-                print("Your health kit has more stuff now!")
-                print("You can use any of this anytime!")
+class Armor(object):
+    def __init__(self, protection, damage):
+        self.protection = protection
+        self.damage = damage
 
-            class Electronics(electronics, on=True, off=False):
-                def __init__(self):
-                    super(Electronics, self).__init__(name)
-                    self.on = True
-                    self.off = False
+class Helmet(Armor):
+    def __init__(self):
+    def equip(self):
+        self.damage -= 5
+        self.health -= 5
+        print("Some damage was done.")
 
-                def microwave(self):
-                    self.on = True
-                    print("The flashlight is on!")
-                    self.off = False
-                    print("The flashlight is off!")
+    def chestplate(self, protection):
+        self.protection = protection
+        self.damage -= 5
+        self.health -= 5
+        print("Some damage was done.")
 
-                def flashlight(self):
-                    self.on = True
-                    print("The flashlight is on!")
-                    self.off = False
-                    print("The flashlight is off!")
+    def shield(self, protection):
+        self.protection = protection
+        self.damage -= 5
+        self.health -= 5
+        print("Some damage was done.")
 
-            class Armor(armor, protection):
-                def __init__(self):
-                    super(Armor, self).__init__(name)
-                    self.protection = protection
-
-                def helmet(self):
-                    self.protection = protection
-                    self.damage -= 5
-                    self.health -= 5
-                    print("Some damage was done.")
-
-                def chestplate(self):
-                    self.protection = protection
-                    self.damage -= 5
-                    self.health -= 5
-                    print("Some damage was done.")
-
-                def shield(self):
-                    self.protection = protection
-                    self.damage -= 5
-                    self.health -= 5
-                    print("Some damage was done.")
-
-                if armor >= damage:
-                    print("No damage is done")
-                elif damage >= armor:
-                    print("There is %s damage done! " % damage)
+    if armor >= damage:
+        print("No damage is done")
+    elif damage >= armor:
+        print("There is %s damage done! " % damage)
 
 
 playing = True
@@ -283,4 +294,3 @@ while playing:
         print("Command Not Found")
 
 
-# this is a comment this is not real code  this a warning and if you look down you would get an F
--- notes/sales records.py
+++ notes/sales records.py
@@ -15,6 +15,8 @@ with open("Sales Records.csv") as old_csv:
     print("The total Fruits profit was:")
     print("${:,}".format(total))
 
+print("------------")
+
 with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
     total = 0
@@ -30,6 +32,8 @@ with open("Sales Records.csv") as old_csv:
     total = round(total, 2)
     print("The total Clothes profit was:")
     print("${:,}".format(total))
+
+print("------------")
 
 with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
@@ -47,6 +51,8 @@ with open("Sales Records.csv") as old_csv:
     print("The total Meat profit was:")
     print("${:,}".format(total))
 
+print("------------")
+
 with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
     total = 0
@@ -63,6 +69,8 @@ with open("Sales Records.csv") as old_csv:
     print("The total Beverages profit was:")
     print("${:,}".format(total))
 
+print("------------")
+
 with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
     total = 0
@@ -78,3 +86,130 @@ with open("Sales Records.csv") as old_csv:
     total = round(total, 2)
     print("The total Office Supplies profit was:")
     print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Cosmetics":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Cosmetics profit was:")
+    print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Snacks":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Snacks profit was:")
+    print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Personal Care":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Personal Care profit was:")
+    print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Household":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Household profit was:")
+    print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Vegetables":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Vegetables profit was:")
+    print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Baby Food":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Baby food profit was:")
+    print("${:,}".format(total))
+
+print("------------")
+
+with open("Sales Records.csv") as old_csv:
+    reader = csv.reader(old_csv)
+    total = 0
+    for row in reader:
+        if row[0] == 'Region':
+            continue
+        # old_number = int(row[0]) + 1
+        old_number = row[13]
+        if row[2] == "Cereal":
+            total += float(old_number)
+            # print(old_number)
+
+    total = round(total, 2)
+    print("The total Cereal profit was:")
+    print("${:,}".format(total))
+
